86a586f60b34d9adb3f2306d5d018654d13063b6
bobagby78 <bobagby78@gmail.com>
2021-04-18 16:48:34 -0500

    Done with all exercises, now moving on to the studio.

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -1,6 +1,27 @@
-//package exercises;
-//
-//import java.util.Scanner;
-//
-//public class Alice {
-//}
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args){
+        String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
+                        "and of having nothing to do: once or twice she had peeped into the book her sister was reading, " +
+                        "but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+        Scanner input = new Scanner(System.in);
+        String searchTerm;
+
+        System.out.println("Give me a search term for the first sentence of \"Alice\" " );
+        searchTerm = input.nextLine().toLowerCase();
+        input.close();
+        if (alice.toLowerCase().contains(searchTerm)){
+            System.out.print("The first occurence of that term is at index " + alice.indexOf(searchTerm) + ". ");
+            String searchTermRemovedStart = alice.substring(0,alice.indexOf(searchTerm)) + "*HERE IT WAS*";
+            String searchTermRemovedEnd = alice.substring(alice.indexOf(searchTerm) + searchTerm.length(), alice.length());
+            System.out.println(searchTermRemovedStart + searchTermRemovedEnd);
+        }else{
+            System.out.println("That term is not in this part of the text.");
+        }
+
+    }
+}
--- src/exercises/MilesPerGallon.java
+++ src/exercises/MilesPerGallon.java
@@ -1,8 +1,21 @@
-//package exercises;
-//
-//import java.util.Scanner;
-//
-//public class MilesPerGallon {
-//    public static void calculateRectangleArea(Float[] args)}
-//
-//}
+package exercises;
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        float miles;
+        float gallons;
+
+        System.out.println("How many miles have you driven? ");
+        miles = input.nextFloat();
+        System.out.println("How many gallons have you burned? ");
+        gallons = input.nextFloat();
+        input.close();
+
+        float mileage = miles/gallons;
+        System.out.println("Your current gas mileage is " + mileage + " miles per gallon");
+    }
+
+}
