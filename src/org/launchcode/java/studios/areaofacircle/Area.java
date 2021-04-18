a5c52dd9ca6a44ce7a1134c5a284ef223720fe86
bobagby78 <bobagby78@gmail.com>
2021-04-18 18:31:22 -0500

    Done with exercises and studio, Bonus missions?

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,17 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double radius;
+
+        System.out.println("Rounded to one decimal place (EX: 4.2), what is the radius of the circle?: ");
+        radius = input.nextDouble();
+        input.close();
+        Double area = Circle.getArea(radius);
+        System.out.println("The area of a circle with a radius of " + radius + " is " + 3.14 * radius * radius);
+    }
+
 }
